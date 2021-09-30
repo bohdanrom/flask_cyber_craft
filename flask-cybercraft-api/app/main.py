caf5134b1d58c3a039f8b1ac32d6dcf64c74f87c
bohdanrom <68371035+bohdanrom@users.noreply.github.com>
2021-09-30 22:52:51 +0300

    Update main.py

--- flask-cybercraft-api/app/main.py
+++ flask-cybercraft-api/app/main.py
@@ -1,8 +1,9 @@
 # Test task(API with GraphQL) for CyberCraft
+import requests
 
 from flask import Flask, render_template, request, jsonify
 from flask_graphql import GraphQLView
-from schema import schema, extract
+from schema import schema
 
 
 app = Flask(__name__)
@@ -13,10 +14,16 @@ def graphql():
     """This function receives data from input field by AJAX and send JSON response"""
     if request.referrer is not None and request.referrer.find("/main") != 1:
         if request.method == "POST":
-            if request.form.get("gitHubLogin"):
-                github_login = request.form.get("gitHubLogin")
-                api_response = extract(url=f"https://api.github.com/users/{github_login}")
-                return jsonify(api_response)
+            url = "https://api.github.com/graphql"
+            query = request.get_data().decode('utf-8')
+            headers = {"Authorization": "token ",
+                       "Content-Type": "application/json"}
+            response = requests.post(url=url, json={"query": query}, headers=headers)
+            response_json = response.json().get("data").get("user")
+            return jsonify({
+                "github_name": response_json.get("name"),
+                "github_repos": [repo.get("name") for repo in response_json.get("repositories").get("nodes")]
+            })
     else:
         return GraphQLView.as_view('graphiql', schema=schema, graphiql=True)()
 
